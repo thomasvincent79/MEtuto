13c1e079ded0f8eaa6c7288ca138ce73c56e5e4c
Thomas <thomas.vincent79.pro@gmail.com>
2020-04-29 11:21:00 +0200

    Premières fonctions

---
+++ R/Firstfonction.R
@@ -0,0 +1,19 @@
+#' Calculs basiques
+#'
+#' @param x un nombre
+#' @param y un nombre
+#'
+#' @return la somme ou le produit de \code{x} et \code{y}
+#' @export
+#'
+#' @examples
+#' Addition(4,7)
+Addition<-function(x,y){
+  x+y
+}
+
+#' @rdname Addition
+Multiplication <- function(x, y){
+  x*y
+}
+
